2491dd05ce87441b901bf2c2c9793b0bb05b68a5
Evgeny Padalka <evgeny.padalka@r46.local>
2024-08-30 16:04:38 +0300

    add service and repository

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,15 @@ import (
 	"log"
 	"ross146/todo-app"
 	"ross146/todo-app/pkg/handler"
+	"ross146/todo-app/pkg/repository"
+	"ross146/todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatal(err)

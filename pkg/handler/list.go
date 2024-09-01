2a926378ff0441ba5de63d7d09e725f3162e8d9f
Evgeny Padalka <evgeny.padalka@r46.local>
2024-09-01 13:56:35 +0300

    add remove list handler

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -72,4 +72,25 @@ func (h *Handler) getListById(c *gin.Context) {
 
 func (h *Handler) updateList(c *gin.Context) {}
 
-func (h *Handler) deleteList(c *gin.Context) {}
+func (h *Handler) deleteList(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid id param")
+		return
+	}
+
+	err = h.services.TodoList.Delete(userId, id)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, &statusResponse{
+		Status: "ok",
+	})
+}
